4e8eda2a7e826643d1933e7c9d1f113acfc065ba
sayaka-mi <sayakakarai@gmail.com>
2025-05-19 16:25:32 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-   validates :nickname, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' }
 end

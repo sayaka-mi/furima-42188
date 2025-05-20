5415a1131bcbb427875bea085445f19527103370
sayaka-mi <sayakakarai@gmail.com>
2025-05-20 14:36:21 +0900

    newアクションcreateアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Failed to create the item."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def message_params
-    params.require(:item).permit(:name, :description, :price, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image, :category_id, :condition_id, :shopping_fee_burden_id, :prefecture_id, :delivery_time_id).merge(user_id: current_user.id)
   end
 end

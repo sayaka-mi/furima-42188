a1645bd33ada05eb59c35417ff72e9f6933f6379
sayaka-mi <sayakakarai@gmail.com>
2025-05-20 09:53:23 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:name, :description, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
 
   validates :name, :description, :category_id, :condition_id, :shopping_fee_burden_id, :prefecture_id, :delivery_time_id, :price, presence: true
+  validates :image, presence: true
 end

d96c2232cfa3637f018d0969f55042b6774e5f7a
sayaka-mi <sayakakarai@gmail.com>
2025-05-20 13:31:27 +0900

    ActiveHash導入・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shopping_fee_burden
+  belongs_to :prefecture
+  belongs_to :delivery_time
 
-  validates :name, :description, :category_id, :condition_id, :shopping_fee_burden_id, :prefecture_id, :delivery_time_id, :price, presence: true
-  validates :image, presence: true
+  validates :image, :name, :description, :condition_id, :shopping_fee_burden_id, :prefecture_id, :delivery_time_id, :price, presence: true
+  validates :category_id, :condition_id, :shopping_fee_burden_id, :prefecture_id, :delivery_time_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end

ab26f9028a9a6e36c2400b8e08df582d362cdca6
sayaka-mi <sayakakarai@gmail.com>
2025-05-28 15:00:13 +0900

    Formオブジェクトを作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,25 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id, :item_id, :city, :block, :building_name
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+
+    Address.create(
+      post_code: post_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      block: block,
+      building_name: building_name,
+      phone_number: phone_number,
+      purchase_id: purchase.id
+    )
+  end
+end

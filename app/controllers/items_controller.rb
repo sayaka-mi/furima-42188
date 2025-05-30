21148545041ccdbe714a48adba4beac4a60def8d
sayaka-mi <sayakakarai@gmail.com>
2025-05-30 15:55:11 +0900

    商品情報編集機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :redirect_if_sold_out, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -53,5 +54,11 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to root_path unless current_user == @item.user
+  end
+
+  def redirect_if_sold_out
+    if @item.purchase.present?
+      redirect_to root_path
+    end
   end
 end
